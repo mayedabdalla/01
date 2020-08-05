3f67487f2ac0766f1a440e2c26fdec07bd27b48b
mayed <mayed@mayeds-MacBook-Pro.local>
2020-08-06 02:18:27 +0400

    ks omk

--- server-demo/dis/generated/graphql.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Type = void 0;
-var Type;
-(function (Type) {
-    Type["Manhwa"] = "MANHWA";
-    Type["Manga"] = "MANGA";
-})(Type = exports.Type || (exports.Type = {}));
--- server-demo/dis/resolvers/File.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var File = {
-    link: function (parent) {
-        return "http://localhost:4000/uploads/" + parent.filename;
-    }
-};
-exports.default = File;
